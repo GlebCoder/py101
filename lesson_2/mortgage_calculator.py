fd579b88361e71986f62d80b55dcbe7b79b83f3f
GlebCoder <gberliand@gmail.com>
2026-03-04 08:33:23 +0300

    make prompt function print and get rid of redundant code in get_valid_input function

--- lesson_2/mortgage_calculator.py
+++ lesson_2/mortgage_calculator.py
@@ -1,5 +1,5 @@
 def prompt(message):
-    return f"==> {message}"
+    print(f"==> {message}")
 
 def is_invalid_int(number):
     try:
@@ -16,42 +16,39 @@ def is_invalid_float(number):
     return number <= 0
 
 def get_monthly_payment(loan_amount, months, annual_rate):
-    loan_amount = float(loan_amount)
-    loan_duration_months = int(months)
-    monthly_rate = float(annual_rate) / 100 / 12
-    monthly_payment = loan_amount * (monthly_rate / (1 - (1 + monthly_rate) ** (-loan_duration_months)))
+    monthly_rate = annual_rate / 100 / 12
+    monthly_payment = loan_amount * (monthly_rate / (1 - (1 + monthly_rate) ** (-months)))
     return monthly_payment
 
 def get_valid_input(msg, validator, error_msg):
-    print(prompt(msg))
+    prompt(msg)
     number = input()
-    if validator(number):
-        while validator(number):
-            print(prompt(error_msg))
-            number = input()
+    while validator(number):
+        prompt(error_msg)
+        number = input()
     return number
 
-print(prompt("Welcome to Mortgage Calculator"))
+prompt("Welcome to Mortgage Calculator")
 while True:
-    loan_amount = get_valid_input("Please enter the loan amount",
+    loan_amount =float(get_valid_input("Please enter the loan amount",
                                   is_invalid_float,
-                                  "You should enter a positive number.")
+                                  "You should enter a positive number."))
 
 
-    months = get_valid_input("Please enter a whole number of months for the loan",
+    months = int(get_valid_input("Please enter a whole number of months for the loan",
                              is_invalid_int,
-                             "You should enter a positive whole number.")
+                             "You should enter a positive whole number."))
 
-    annual_rate = get_valid_input("Please enter the annual rate",
+    annual_rate = float(get_valid_input("Please enter the annual rate",
                                   is_invalid_float,
-                                  "You should enter a positive number.")
+                                  "You should enter a positive number."))
 
     monthly_payment = get_monthly_payment(loan_amount, months, annual_rate)
-    print(prompt(f"Your monthly payment for the loan is: ${monthly_payment:.2f}"))
+    prompt(f"Your monthly payment for the loan is: ${monthly_payment:.2f}")
 
-    print(prompt("Another calculation? Please enter yes or no."))
+    prompt("Another calculation? Please enter yes or no.")
     answer = input().casefold()
-    if answer[0].casefold() != "y":
+    if not answer or answer[0].casefold() != "y":
         break
 
 
