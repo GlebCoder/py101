a0d288d8b68175dafaec222ff392867f6a6f741d
GlebCoder <gberliand@gmail.com>
2026-03-05 21:53:06 +0300

    add scenario logic to get_valid_choice

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -29,16 +29,29 @@ def prompt(message):
 
 def display_valid_choices():
     list_of_valid_choices = []
-    for key in MAP_CHOICES:
-        choice = f"{MAP_CHOICES[key]}({key})"
+    for key in WINS_AGAINST:
+        if key[0] == "s":
+            choice = f"{key}({key[:2]})"
+        else:
+            choice = f"{key}({key[0]})"
         list_of_valid_choices.append(choice)
     prompt(", ".join(list_of_valid_choices))
 
 
-def get_valid_choice():
-    prompt("It's not a valid choice!")
-    choice = input().casefold().strip()
-    return choice
+def get_valid_choice(scenario):
+    if scenario == "play":
+        choice = input().casefold().strip()
+        while choice not in MAP_CHOICES.keys():
+            prompt("It's not a valid choice!")
+            choice = input().casefold().strip()
+        return MAP_CHOICES[choice]
+    else:
+        choice = input().casefold().strip()
+        while not choice or (choice[0] != "y" and choice[0] != "n"):
+            prompt("It's not a valid choice! You must choose 'y' or 'n'")
+            choice = input().casefold().strip()
+        return choice
+
 
 
 def find_winner(choice, computer_choice):
@@ -72,15 +85,12 @@ while True:
     your_wins = 0
     computer_wins = 0
     while your_wins < WINS_SCORE and computer_wins < WINS_SCORE:
-        prompt("Choose one: rock(r), paper(p), scissors(sc), lizard(l), spock(sp)")
+        #prompt("Choose one: rock(r), paper(p), scissors(sc), lizard(l), spock(sp)")
+        prompt("Choose one:")
+        display_valid_choices()
 
-        choice = input().casefold().strip()
-
-        while choice not in MAP_CHOICES.keys():
-           choice = get_valid_choice()
-
-        choice = MAP_CHOICES[choice]
-        computer_choice = random.choice(list(MAP_CHOICES.values()))
+        choice = get_valid_choice("play")
+        computer_choice = random.choice(list(WINS_AGAINST.keys()))
 
         prompt(f"You chose {choice}, computer chose {computer_choice}")
         game_result = find_winner(choice, computer_choice)
@@ -88,8 +98,7 @@ while True:
 
         if game_result == "computer":
             computer_wins += 1
-
-        if game_result == "you":
+        elif game_result == "you":
             your_wins += 1
 
         prompt(f"The game score: your wins {your_wins} and computer wins {computer_wins}")
@@ -100,9 +109,7 @@ while True:
         prompt("Match is over. Computer wins!")
 
     prompt(f"Play again? (y/n)")
-    answer = input().casefold().strip()
-    while not answer or (answer[0] != "y" and answer[0] != "n"):
-        answer = get_valid_choice()
+    answer = get_valid_choice("continue")
 
     if answer[0] == "n":
         break
